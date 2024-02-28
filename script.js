f2d4fc67f41b6cfa43f43a973b4b46aaf9839454
Pushpendra Baswal <pushpendra9122@gmail.com>
2024-02-28 13:05:11 +0530

    update

--- script.js
+++ script.js
@@ -9,9 +9,7 @@ function isMobileDevice() {
 // Function to display a prompt to switch to desktop mode
 function showDesktopPrompt() {
   if (isMobileDevice()) {
-    var response = confirm(
-      "For better experience, switch to desktop mode. Do you want to proceed?"
-    );
+    var response = confirm("It Only Run On Desktop !");
     if (response == true) {
       // Redirect to desktop version of the site
       window.location.href = "https://jiteshthakurweb.vercel.app";
